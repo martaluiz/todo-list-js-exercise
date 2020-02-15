8d527eaaeff94ed3b20db0c6750ae8921a244db1
Marta Luiz Avelino <martaluizavelino@gmail.com>
2020-02-15 00:45:02 +0000

    refactor and Use Methods

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-
 function newTask(title, description) {
   const task = {
     title: title,
     description: description,
     complete: false,
-    completeTask: function() {
-      this.complete = true;
-    },
-    logTaskState: function() {
-      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
-    }
 
-  }
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    }
+  };
   return task;
 }
 
+// DRIVER CODE CHANGES BELOW
 
-// DRIVER CODE BELOW
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
 
-task1.logTaskState();
-task1.completeTask();
-console.log('task1', task1.title);
+task1.title; // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logState(); // Clean Cat Litter has been completed
